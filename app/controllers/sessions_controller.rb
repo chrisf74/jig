d68e7f00eaa9f1bc485da0659ab23f0d70984a68
Chris Forrest <chrisf74@gmail.com>
2015-06-02 14:18:58 -0400

    Session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,13 @@
 class SessionsController < ApplicationController
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in(user)
+      redirect_to home_url
+    else
+      flash.now[:danger] = 'Wrong user name or password'
+      render 'new'
+    end
   end
   def new
   end
